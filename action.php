29b2af52576b3fe71a4da502db10bd3a1c60b577
Sh Pi <abhinuvpitale@gmail.com>
2018-10-18 02:03:33 +0000

    php action php

--- action.php
+++ action.php
@@ -1,8 +1,11 @@
 <?php
  $path = 'data.txt';
+ echo $path;
  if (isset($_POST['field1']) && isset($_POST['field2'])) {
-    $fh = fopen($path,"a+");
+    $fh = fopen($path,"w");
+    echo $fh;
     $string = $_POST['field1'].' - '.$_POST['field2'];
+    echo $string;
     fwrite($fh,$string); // Write information to the file
     fclose($fh); // Close the file
  }
